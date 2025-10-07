1c3aa8224f86430e3f6a8a210f2660bbe766e2c2
Sengoku11 <batyr.sengoku@gmail.com>
2025-10-07 22:28:02 +0800

    chore: highlight with

--- queries/haskell/highlights.scm
+++ queries/haskell/highlights.scm
@@ -1,12 +1,12 @@
 ;; extends
-;; DAML-on-Haskell keywords without using huge `_` captures
+;; DAML-on-Haskell keywords
 
-; Type-ish keywords you want colored like keywords
+;; Treat these like `data` (type-ish keywords)
 ((variable) @keyword
   (#any-of? @keyword
-   "template" "interface" "choice" "nonconsuming" "preconsuming" "postconsuming"))
+   "template" "interface" "choice" "nonconsuming" "preconsuming" "postconsuming" "with"))
 
-; Builtin-ish DSL words you want colored like functions
+;; Treat these like functions
 ((variable) @function.builtin
   (#any-of? @function.builtin
    "controller" "signatory" "viewtype" "observer" "ensure" "this" "arg" "self"))
