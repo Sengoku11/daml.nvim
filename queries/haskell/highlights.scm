7cde6460c471185d683f358d87ece128e48d3b92
Sengoku11 <batyr.sengoku@gmail.com>
2025-10-07 22:43:59 +0800

    chore: extra keywords and builtins for highlighting

--- queries/haskell/highlights.scm
+++ queries/haskell/highlights.scm
@@ -4,9 +4,9 @@
 ;; Treat these like `data` (type-ish keywords)
 ((variable) @keyword
   (#any-of? @keyword
-   "template" "interface" "choice" "nonconsuming" "preconsuming" "postconsuming" "with"))
+   "template" "interface" "choice" "nonconsuming" "preconsuming" "postconsuming" "with" "do" "where"))
 
-;; Treat these like functions
+;; Treat these like builtin functions
 ((variable) @function.builtin
   (#any-of? @function.builtin
-   "controller" "signatory" "viewtype" "observer" "ensure" "this" "arg" "self"))
+   "controller" "signatory" "viewtype" "observer" "ensure" "this" "arg" "self" "submit" "exercise" "create"))
